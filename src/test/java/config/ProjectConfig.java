5d95bce8f17a1ddd48ff8a2c36d499f06129faf7
DaymianDark <nightkonn@gmail.com>
2022-02-19 14:29:30 +0300

    ProjectConfig changed

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -2,8 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-//        "system:properties",
+        "system:properties",
         "classpath:config/local.properties"
 })
 public interface ProjectConfig extends Config {
