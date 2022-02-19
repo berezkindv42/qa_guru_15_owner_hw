1983b67378c9279e57c06569df8e6960771796bf
DaymianDark <nightkonn@gmail.com>
2022-02-19 11:57:33 +0300

    added owner task

--- src/test/java/berezkindv/owner/OwnerTests.java
+++ src/test/java/berezkindv/owner/OwnerTests.java
@@ -2,6 +2,7 @@ package berezkindv.owner;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -11,6 +12,7 @@ public class OwnerTests {
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @Test
+    @Tag("owner")
     void loginTest() {
         String login = config.login();
         String password = config.password();
