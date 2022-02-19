e1c9b32ed909278afeb6ce4d9984bc648d583963
DaymianDark <nightkonn@gmail.com>
2022-02-19 12:46:47 +0300

    owner, properties tasks deleted

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -2,7 +2,10 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources("classpath:config/credentials.properties")
+@Config.Sources({
+        "system:properties",
+        "classpath:config/credentials.properties"
+})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
