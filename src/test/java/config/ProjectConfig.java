b4aa1cb0cae5ba84280fa94e2c04ccc39eb03d33
DaymianDark <nightkonn@gmail.com>
2022-02-19 13:27:41 +0300

    default settings commented

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -5,11 +5,11 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:config/local.properties")
 public interface ProjectConfig extends Config {
 
-    @DefaultValue("chrome")
+//    @DefaultValue("chrome")
     String browser();
-    @DefaultValue("91.0")
+//    @DefaultValue("91.0")
     String browserVersion();
-    @DefaultValue("1920x1080")
+//    @DefaultValue("1920x1080")
     String browserSize();
     String remoteDriverUrl();
 }
